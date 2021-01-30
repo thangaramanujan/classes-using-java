c6e73d52dbc7232f521e09dc56e66c504266a803
thangaramanujan <77881820+thangaramanujan@users.noreply.github.com>
2021-01-30 20:14:36 +0530

    -Car class is used

---
+++ src/Study/programming/Car.java
@@ -0,0 +1,23 @@
+package Study.programming;
+
+public class Car {
+
+    private  int doors;
+    private  int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if(validModel.equals("porsche") || validModel.equals("carrera")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ src/Study/programming/Main.java
@@ -0,0 +1,11 @@
+package Study.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("carrera");
+        System.out.println("Model is " + porsche.getModel());
+           }
+}
